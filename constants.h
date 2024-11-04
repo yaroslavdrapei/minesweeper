2719e928b6b0dfad971d5a679c6ff976e190dddc
yaroslavdrapei <yarikdrapey@gmail.com>
2024-11-04 23:07:03 +0200

    feat(render): test render without oop

--- constants.h
+++ constants.h
@@ -1,4 +1,6 @@
 #pragma once
 
-constexpr int GRID_WIDTH = 15;
-constexpr int GRID_HEIGHT = 10;
+constexpr int GRID_WIDTH = 25;
+constexpr int GRID_HEIGHT = 15;
+constexpr int CELL_SIZE = 40;
+constexpr int MINES_COUNT = 100;
